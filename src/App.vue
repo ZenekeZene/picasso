d2873522dc854f5387ded819ceb54d2c227619d2
Héctor <hvillar@theinit.com>
2019-07-24 13:03:31 +0200

    Add anchor on github link

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <main class="app">
 	<h1 class="heading">Picass<span class="icon-heart"></span></h1>
     <router-view/>
-	<github-item href="https://github.com/ZenekeZene/picasso.git"></github-item>
+	<github-item href="https://github.com/ZenekeZene/picasso#picasso"></github-item>
   </main>
 </template>
 
