8e35cb050bb9d280e83549d4aa875196b6c20fdb
Héctor <hvillar@theinit.com>
2019-07-26 14:47:45 +0200

    Testing dinamically Text Input

---
+++ src/components/TextInput.vue
@@ -0,0 +1,14 @@
+<template>
+	<p contenteditable="true" class="text draggable"
+	v-touch:longtap="longtap"></p>
+</template>
+<script>
+export default {
+	name: 'TextInput',
+	methods: {
+		longtap() {
+			alert('longtap');
+		},
+	},
+}
+</script>
