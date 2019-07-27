5969505dda114fd41116191ffb69071cdf8050ac
Héctor <hvillar@theinit.com>
2019-07-28 01:23:26 +0200

    Delete modal added, spinner

---
+++ src/components/ModalDelete.vue
@@ -0,0 +1,30 @@
+<template>
+<modal name="modal-delete" :adaptive="true" :pivotY="0" height="auto">
+	<div slot="top-right">
+		<span @click="$modal.hide('modal-delete')" class="icon-cross"></span>
+	</div>
+	<h3 class="--hot">Borrar usuario</h3>
+	<p>¿De verdad quieres borrar el siguiente usuario?</p>
+	<p font-bold margin-zero padding-left>{{ user.name }}</p>
+	<p font-bold padding-left>{{ user.email }}</p>
+	<button margin-top-2 class="btn --hot" @click="$emit('deleteUser', user)">Borrar usuario</button>
+</modal>
+</template>
+
+<script>
+export default {
+	name: 'ModalDelete',
+	props: {
+		user: {
+			type: Object,
+			default: () => {
+				return {
+					name: '',
+					email: ''
+				};
+			},
+		},
+	},
+};
+</script>
+
---
+++ src/components/SpinnerItem.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="lds-ripple"><div></div><div></div></div>
+</template>
+
+<script>
+export default {
+	name: 'SpinnerItem',
+};
+</script>
