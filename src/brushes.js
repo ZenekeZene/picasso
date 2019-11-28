9084420edf9597d7935e86dea8a37a847a4b41e6
ZenekeZene <hectorvillarm@gmail.com>
2019-11-28 17:03:54 +0100

    Brushes initial added

---
+++ src/brushes.js
@@ -0,0 +1,35 @@
+const brushes = [
+	{
+		key: 'normal',
+		iconClassname: 'pencil',
+		type: 'simple',
+	},
+	{
+		key: 'neighbor',
+		iconClassname: 'pen',
+		type: 'neighbor',
+		config: {
+			numNeighbors: 5,
+		},
+	},
+	{
+		key: 'spray',
+		iconClassname: 'sprayer',
+		type: 'neighbor',
+		config: {
+			numNeighbors: 20,
+		},
+	},
+	{
+		key: 'format',
+		iconClassname: 'paint-format',
+		type: '',
+	},
+	{
+		key: 'quill',
+		iconClassname: 'quill',
+		type: '',
+	},
+];
+
+export default brushes;
---
+++ src/components/tools/BrushTool.vue
@@ -0,0 +1,52 @@
+<template>
+	<li class="tools__item" v-if="mode === 'edit'">
+		<span
+			class="icon"
+			v-show="!toolsVisible"
+			:class="{
+				'--disabled': isPlaying || isPainting || mode === 'read'
+			}"
+		></span>
+		<ol class="colors --brushes" v-show="toolsVisible">
+			<li
+				v-for="(brusher, index) in brushes"
+				:key="`brush-${index}`"
+				@click="setBrush({ brush: brusher })"
+				:class="{ '--selected': brush.key === brusher.key }"
+			>
+				<span :class="`icon-${brusher.iconClassname}`" v-mobile-hover:#4992a9></span>
+			</li>
+		</ol>
+	</li>
+</template>
+<script>
+import brushes from '../../brushes';
+import { mapState, mapMutations } from 'vuex';
+export default {
+	name: 'BrushTool',
+	props: {
+		toolsVisible: {
+			default: false,
+			type: Boolean,
+		},
+	},
+	computed: {
+		...mapState([
+			'isPainting',
+			'isPlaying',
+			'mode',
+			'brush',
+		]),
+	},
+	data() {
+		return {
+			brushes: brushes,
+		};
+	},
+	methods: {
+		...mapMutations([
+			'setBrush',
+		]),
+	},
+}
+</script>
