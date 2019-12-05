2b07f808ccecdfb7b794c44c3cb7da5d8543eff4
ZenekeZene <hectorvillarm@gmail.com>
2019-12-05 12:55:16 +0100

    PlayerDot more simple

--- src/mixins/PlayerDot.mixin.js
+++ src/mixins/PlayerDot.mixin.js
@@ -1,6 +1,6 @@
 import { mapState } from 'vuex';
 
-var playerDot = {
+const playerDot = {
 	computed: {
 		...mapState([
 			'history',
@@ -20,6 +20,13 @@ var playerDot = {
 			const offsetX = dot.pmousex;
 			const offsetY = dot.pmousey;
 			this.ctx.beginPath();
+			this.applyStyles(dot);
+			this.ctx.moveTo(x, y);
+			this.ctx.lineTo(offsetX, offsetY);
+			this.ctx.stroke();
+			this.prevPosition = { offsetX, offsetY };
+		},
+		applyStyles(dot) {
 			this.ctx.lineWidth = dot.size;
 			this.ctx.strokeStyle = dot.color;
 			if (this.theme === 'light' && dot.color === 'rgb(39, 39, 39)') {
@@ -30,19 +37,9 @@ var playerDot = {
 			) {
 				this.ctx.strokeStyle = 'rgb(39, 39, 39)';
 			}
-			this.ctx.moveTo(x, y);
-			this.ctx.lineTo(offsetX, offsetY);
-			this.ctx.stroke();
-			this.prevPosition = { offsetX, offsetY };
 		},
 		player() {
-			for (let i = 0; i < this.history.length; i += 1) {
-				const stroke = this.history[i];
-				for (let j = 0; j < stroke.length; j += 1) {
-					const dot = stroke[j];
-					this.paintDot(dot);
-				}
-			}
+			this.history.map((stroke) => stroke.map((dot) => this.paintDot(dot)));
 		},
 	},
 };
