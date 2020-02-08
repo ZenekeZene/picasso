db6fe593c07a9dcdb5f39c012889f40dfa0c048c
ZenekeZene <hectorvillarm@gmail.com>
2020-02-08 13:39:31 +0100

    Testing alert

--- src/components/KanvasItem.vue
+++ src/components/KanvasItem.vue
@@ -92,6 +92,7 @@ export default {
                 (event) => {
                     const t1 = event.touches[0];
                     const t2 = event.touches[1];
+                    alert(t1, t2);
 
                     if (t1 && t2) {
                         event.preventDefault();
@@ -138,6 +139,7 @@ export default {
                 'touchend',
                 () => {
                     this.lastDist = 0;
+                    alert('touchend');
                     this.point = undefined;
                 },
                 false
